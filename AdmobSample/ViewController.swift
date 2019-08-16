d4ed6fb6199465759ff215ad54a46b4de2ee8d00
obscure723 <obscure723@yone-lab.com>
2019-08-16 21:28:08 +0800

    no message

--- AdmobSample/ViewController.swift
+++ AdmobSample/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    // 広告ユニットID
+    let AdMobID = "[Your AdMob ID]"
+    // テスト用広告ユニットID
+    let TEST_ID = "ca-app-pub-3940256099942544/2934735716"
+    
+    // true:テスト
+    let AdMobTest:Bool = true
+    
+    @IBOutlet weak var admobView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("Google Mobile Ads SDK version: \(GADRequest.sdkVersion())")
+        
+        if AdMobTest {
+            admobView.adUnitID = TEST_ID
+        } else {
+            admobView.adUnitID = AdMobID
+        }
+        
+        admobView.rootViewController = self
+        
+        admobView.load(GADRequest())
+
     }
 
-
 }
-

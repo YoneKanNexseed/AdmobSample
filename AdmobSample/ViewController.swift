f6ccf6ea4a6f314e4df071e56bbc82a889dffeb3
obscure723 <obscure723@yone-lab.com>
2019-08-16 20:25:37 +0800

    first commit

---
+++ AdmobSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdmobSample
+//
+//  Created by yonekan on 2019/08/16.
+//  Copyright © 2019 yonekan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
